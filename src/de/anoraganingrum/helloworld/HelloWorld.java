ec5849d5bde10a44c9c852c24ac9b2acc3da2036
Alexander Kriegisch <Alexander@Kriegisch.name>
2013-10-27 09:31:22 +0100

    Add prompt to enter name
    Known bug: cannot exit input loop

--- src/de/anoraganingrum/helloworld/HelloWorld.java
+++ src/de/anoraganingrum/helloworld/HelloWorld.java
@@ -1,33 +1,35 @@
 package de.anoraganingrum.helloworld;
+
 import java.io.IOException;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-                      tellYourName();
-    }
+	public static void main(String[] args) {
+		sayHello("master");
+		tellYourName();
+	}
 
+	public static void sayHello(String name) {
+		System.out.println("Hello " + name + "!");
+	}
 
-    public static void sayHello(String name) {
-        System.out.println("Hello " + name + "!");
-    }
-    public static void tellYourName() {
-        byte buffer[] = new byte[80];  // Zeichenpuffer
-        String input = "";
-        int read;
-        do {
-            try {
-                // Einlesen der Zeichen
-                read = System.in.read(buffer, 0, 80);
-                // Umwandeln des Pufferinhaltes in einen String
-                input = new String(buffer, 0, read);
-                // Ausgabe der eingelesenen Zeichen
-                System.out.print(input);
-            }
-            catch(IOException e) {
-                e.printStackTrace();
-            }
-        } while(! input.equals("exit"+System.getProperty("line.separator")));
-        System.out.println(
-                "Thank you very much for using this program");
-    }
+	public static void tellYourName() {
+		byte buffer[] = new byte[80];  // Zeichenpuffer
+		String input = "";
+		int read;
+		do {
+			System.out.print("Please enter your name: ");
+			try {
+				// Einlesen der Zeichen
+				read = System.in.read(buffer, 0, 80);
+				// Umwandeln des Pufferinhaltes in einen String
+				input = new String(buffer, 0, read);
+				// Ausgabe der eingelesenen Zeichen
+				System.out.print(input);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} while (!input.equals("exit" + System.getProperty("line.separator")));
+		System.out.println(
+			"Thank you very much for using this program");
+	}
 }
