5e39f996f676ad78f492d8bcc6469d832b7e9b55
Alexander Kriegisch <Alexander@Kriegisch.name>
2013-11-05 15:52:54 +0100

    Auto-reformat whitespace, e.g. tabs vs. spaces

--- src/main/de/anoraganingrum/helloworld/HelloWorld.java
+++ src/main/de/anoraganingrum/helloworld/HelloWorld.java
@@ -14,8 +14,8 @@ public class HelloWorld {
 		sayHello("master");
 		testAllCiphers();
 		tellYourName();
-        tellTimeToMeet();
-		System.out.println("Selamat "+timeDependantGreeting());
+		tellTimeToMeet();
+		System.out.println("Selamat " + timeDependantGreeting());
 	}
 
 	public static void sayHello(String name) {
@@ -55,70 +55,70 @@ public class HelloWorld {
 		System.out.println("Hi " + input + "!");
 	}
 
-    public static void tellTimeToMeet() throws IOException {
-        byte buffer[] = new byte[80];
-        String time;
-        int read;
-        System.out.print("Please enter time [hh:mm] to meet me: ");
+	public static void tellTimeToMeet() throws IOException {
+		byte buffer[] = new byte[80];
+		String time;
+		int read;
+		System.out.print("Please enter time [hh:mm] to meet me: ");
 		read = System.in.read(buffer, 0, 80);
 		time = parseTime(new String(buffer, 0, read - 1));
 		System.out.println("OK, at " + time + " you will meet me.");
-    }
+	}
 
 	public static String parseTime(String time) throws IllegalArgumentException {
-        if (time == null)
-            throw new IllegalArgumentException("parameter must be != null!");
+		if (time == null)
+			throw new IllegalArgumentException("parameter must be != null!");
 
-        time = time.trim();
-        int timeLength = time.length();
+		time = time.trim();
+		int timeLength = time.length();
 
-        if (timeLength == 0)
-            throw new IllegalArgumentException("parameter must not be empty!");
+		if (timeLength == 0)
+			throw new IllegalArgumentException("parameter must not be empty!");
 
-        if (timeLength < 4)
-            throw new IllegalArgumentException("parameter should have format hh:mm");
+		if (timeLength < 4)
+			throw new IllegalArgumentException("parameter should have format hh:mm");
 
-        if (timeLength > 5)
-            throw new IllegalArgumentException("parameter should have format hh:mm");
+		if (timeLength > 5)
+			throw new IllegalArgumentException("parameter should have format hh:mm");
 
-        String[] timeText  = time.split(":");
-        String hoursText   = timeText[0];
-        String minutesText = timeText[1];
+		String[] timeText = time.split(":");
+		String hoursText = timeText[0];
+		String minutesText = timeText[1];
 
-        if (minutesText.length() ==1)
-            throw new IllegalArgumentException("minutes-parameter should have format mm");
+		if (minutesText.length() == 1)
+			throw new IllegalArgumentException("minutes-parameter should have format mm");
 
-        int hours = Integer.parseInt(hoursText);
-        int minutes = Integer.parseInt(minutesText);
-        int hoursLength = Integer.toString(hours).length();
+		int hours = Integer.parseInt(hoursText);
+		int minutes = Integer.parseInt(minutesText);
+		int hoursLength = Integer.toString(hours).length();
 
-        if (hours != Math.abs(hours) || minutes != Math.abs(minutes))
-            throw new IllegalArgumentException("illegal - character");
+		if (hours != Math.abs(hours) || minutes != Math.abs(minutes))
+			throw new IllegalArgumentException("illegal - character");
 
-        if (hoursLength == 1)
-            hoursText = "0" + Integer.toString(hours) ;
+		if (hoursLength == 1)
+			hoursText = "0" + Integer.toString(hours);
 
-        time = hoursText + ":" + minutesText;
-        if (hours >= 24)
-            throw new IllegalArgumentException("hours must be < 24");
-        if (minutes >= 60)
-            throw new IllegalArgumentException("minutes must be < 60");
+		time = hoursText + ":" + minutesText;
+		if (hours >= 24)
+			throw new IllegalArgumentException("hours must be < 24");
+		if (minutes >= 60)
+			throw new IllegalArgumentException("minutes must be < 60");
 
-        return time;
+		return time;
 	}
 
 	public static String timeDependantGreeting() {
-        Calendar cal = Calendar.getInstance();
-        cal.getTime();
-        int hours;
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        System.out.println( "Sekarang ini jam " + sdf.format(cal.getTime()) );
-        hours = cal.getTime().getHours() ;
-        if (hours < 5) return "Subuh";
-        if (hours < 12) return "Pagi";
-        if (hours < 15) return "Siang";
-        if (hours < 18) return "Sore";
-        if (hours < 23) return "Malam";
-        return "Tidur";
-    }
+		Calendar cal = Calendar.getInstance();
+		cal.getTime();
+		int hours;
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		System.out.println("Sekarang ini jam " + sdf.format(cal.getTime()));
+		hours = cal.getTime().getHours();
+		if (hours < 5) return "Subuh";
+		if (hours < 12) return "Pagi";
+		if (hours < 15) return "Siang";
+		if (hours < 18) return "Sore";
+		if (hours < 23) return "Malam";
+		return "Tidur";
+	}
 }
